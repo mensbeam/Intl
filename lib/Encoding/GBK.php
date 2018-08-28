766643aa378142166717b053aab7c2aabeecd989
J. King <jking@jkingweb.ca>
2018-08-28 08:37:32 -0400

    Common infrstructure for gb18030 and GBK

---
+++ lib/Encoding/GBK.php
@@ -0,0 +1,24 @@
+<?php
+/** @license MIT
+ * Copyright 2018 J. King et al.
+ * See LICENSE and AUTHORS files for details */
+
+declare(strict_types=1);
+namespace MensBeam\Intl\Encoding;
+
+class GBK extends GBCommon {
+    const NAME = "GBK";
+    const LABELS = [
+        "chinese",
+        "csgb2312",
+        "csiso58gb231280",
+        "gb2312",
+        "gb_2312",
+        "gb_2312-80",
+        "gbk",
+        "iso-ir-58",
+        "x-gbk",
+    ];
+
+    const GBK = true;
+}
